63da083b673f67b8166b9b0ed0a721a35d68a9cc
jamieellen <jamieellen.w@gmail.com>
2023-09-22 18:18:37 -0700

    Oh Boy

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Item = props => {
   return (
-    <div className={`item${props.item.purchased ? ' purchased' : ''}`}>
+    <div className={`item${props.item.purchased ? ' purchased' : ''}`}
+    onClick={() => {props.toggleItem(props.item.id)}}>
       <p>{props.item.name}</p>
     </div>
   );
 };
 
 export default Item;
+
+//ALSO LOOKS LIKE THIS IN CLASS COMPONENT FORM:
+
+//class Item extends React.component {
+//  render() {
+//    return(<h1>Hello</h1>)    
+//  } 
+//}
--- followAlong/src/components/ListForm.js
+++ followAlong/src/components/ListForm.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 class ListForm extends React.Component {
   // Constructor with state
+  constructor() {
+    super();
+    this.state = {
+      item: ''
+
+    }
+  }
 
   handleChanges = e => {
     // update state with each keystroke
+    e.preventDefault();
+    this.setState({...this.state, item: e.target.value})
   };
 
   // class property to submit form
+  submitForm = e => {
+    e.preventDefault();
+    this.props.addItem(e, this.state.item);
+    this.setState({...this.state, item: ''})
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitForm}>
         {/* This is an uncontrolled component 😬 We want it to be controlled by state */}
-        <input type="text" name="item" />
+        <input type="text" name="item" value={this.state.item} onChange={this.handleChanges}/>
         <button>Add</button>
       </form>
     );
@@ -21,3 +35,25 @@ class ListForm extends React.Component {
 }
 
 export default ListForm;
+
+//ALSO LOOKS LIKE THIS IN FUNCTIONAL COMPONENT FORM:
+
+// const ListForm = props => {
+//   return(
+//     <form>
+//       {/* This is an uncontrolled component 😬 We want it to be controlled by state */}
+//       <input type="text" name="item" />
+//       <button>Add</button>
+//     </form>
+//   )
+// }
+
+
+//Instead of this:
+// const ListForm = () => {
+//   const [item, setItem] = useState(null);
+//   const updateItem = (i) => {
+//     setItem(i);
+//   } 
+// }
+//REFER TO THIS.STATE OBJECT
